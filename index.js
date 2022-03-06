3ab1d21c1ab65409fa6416e398aae70f901da3f5
Chudi <chudionubogu@yahoo.com>
2022-03-06 15:18:47 -0600

    html and css are complete, hopefully i have free time to write js at work

---
+++ index.js
@@ -0,0 +1,23 @@
+const stuff = document.getElementsByID('boxes')
+const playerX = 'X'
+const playerO = 'O'
+let turn
+
+
+for(let i=0; i < stuff.length; i++) {
+    box.addEventListener('click', clicked)
+}
+
+function clicked(x) {
+    display: playerX
+}
+
+
+
+// html and css should be fine i think
+// Need to set up turn calculations
+// need to fix event listener
+// ask alleyn if he can help me figure out how to set up my js
+// need to make sure one player cant overwrite another
+// I dont know how to display a button at the end of the game. maybe some kind of event listener that fires when a winning function is ran
+//
